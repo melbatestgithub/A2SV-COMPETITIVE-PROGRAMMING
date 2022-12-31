835b931b262e259588d233d49267424fa7412642
melbatestgithub <116151311+melbatestgithub@users.noreply.github.com>
2023-01-01 02:29:13 +0300

    Create 2089. Find Target Indices.cpp

---
+++ 2089. Find Target Indices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+       vector<int>a;
+       int n=nums.size();
+       sort(nums.begin(),nums.end());
+       for(int i=0;i<n;i++){
+           if(nums[i]==target){
+              a.push_back(i);
+           }
+       }
+       return a;
+        
+    }
+};
